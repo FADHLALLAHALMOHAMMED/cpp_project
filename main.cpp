8f2e42b629dfb436c826245b2ec630197bf9d08f
Fadhlallah Almohammed <2230006097@iau.edu.sa>
2024-04-25 13:31:48 +0300

    initial -

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <fstream>
 using namespace std;
 
-
-char menu();
 
 struct staffInfo {
 
@@ -18,17 +17,25 @@ struct staffInfo {
 };
 
 
+char menu();
+void loadData(string, staffInfo, int&);
+void displayRecords(string, staffInfo [], int&);
+
 int main() {
+
+    const string staffFile = "staff_info.txt";
 
     const int LIMIT = 1000;
     int noOfStf = 0;
-    int stfDetails[LIMIT];
+    staffInfo stfDetails[LIMIT];
     int userChoice(0);
 
 
     cout << "------- Welcome to the Staff System -------";
 
     userChoice = menu();
+
+    displayRecords(staffFile, stfDetails, noOfStf);
 
 
 
@@ -62,6 +69,52 @@ char menu() {
             cout << "\nInvalid input. Please enter a correct number!\n";
         
         }
+    }
+}
+
+
+void loadData(string fileName, staffInfo stfDetails[], int& noOfStaff) {
+
+    ifstream staffFile(fileName);
+
+    if (staffFile.is_open()) {
+        int count(0);
+
+        while (!staffFile.eof()) {
+            getline(staffFile, stfDetails[count].name, ',');
+            getline(staffFile, stfDetails[count].id, ',');
+            getline(staffFile, stfDetails[count].position, ',');
+            getline(staffFile, stfDetails[count].department, ',');
+            getline(staffFile, stfDetails[count].email, ',');
+            staffFile >> stfDetails[count].age;
+            staffFile.ignore(1);
+            staffFile >> stfDetails[count].salary;
+            staffFile.ignore(1);
+
+            count++;
+        }
+        staffFile.close();
+        noOfStaff = count;
+    }
+}
+
+
+void displayRecords(string fileName, staffInfo stfDetails[], int& noOfStaff) {
+    
+    loadData(fileName, stfDetails, noOfStaff);
+
+    cout << "\nEmployees Info: \n\n";
+
+    for (int i = 0; i < noOfStaff; i++) {
+        
+        cout << "Name: " << stfDetails[i].name << "\n"
+             << "ID number: " << stfDetails[i].id << "\n"
+             << "Position: " << stfDetails[i].position << "\n"
+             << "Department: " << stfDetails[i].department << "\n"
+             << "Email: " << stfDetails[i].email << "\n"
+             << "Age: " << stfDetails[i].age << "\n"
+             << "Salary: " << stfDetails[i].salary << "\n\n"
+             << "---------------------------------------" << "\n\n";
     }
 }
 
